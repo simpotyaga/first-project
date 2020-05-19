2eafd9834a0b9f80351e0dc7b0c575bb17e59ca8
Vladislav <powerpapa62@gmail.com>
2020-05-19 16:13:23 +0300

    first commit

---
+++ notaciYA.js
@@ -0,0 +1,11 @@
+const obj = {
+    name: "Vlad",
+    age: 25,
+    man: true
+};
+
+console.log(obj.name);        //в объекте через точку мы обращаемся на прямую к "name"
+
+let arr = ['plum.png', 'orange.png', 6, {}, []];
+
+console.log(arr[1]);        //в масиве через [] мы обращаемся на прямую к елементу 1
---
+++ script.js
@@ -0,0 +1,42 @@
+// let answers = [];
+
+
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Как ваша фамилия?", "");
+// answers[2] = prompt("Сколько вам лет", "");
+
+
+// document.write(answers);
+
+
+let answers = [];
+let questions = [
+    "Как ваше имя?",
+    "Как ваша фамилия?",
+    "Сколько вам лет"
+];
+
+
+// for (let i = 0; i < questions.length; i++) {
+// answers[i] = prompt(questions[i]);
+// }
+
+// console.log(answers);
+
+let i = 0;
+do {
+answers[i] = prompt(questions[i]);
+i++;
+}
+while (i < questions.length );
+
+
+
+// let i = 0;
+// while (i < questions.length) {
+//     answers[i] = prompt(questions[i]);
+//     i++;
+// }
+
+
+console.log(answers);
